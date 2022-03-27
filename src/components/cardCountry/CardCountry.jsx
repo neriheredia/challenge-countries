c4005dd998048260d74b7642f2c67ccccf311286
neriheredia <developer.heredia@gmail.com>
2022-03-27 17:41:20 -0300

    datos en card recibidos

--- src/components/cardCountry/CardCountry.jsx
+++ src/components/cardCountry/CardCountry.jsx
@@ -1,22 +1,38 @@
 import React from "react"
 import "./cardCountry.scss"
+import { emojiUnicode } from "../../helpers/emojiUnicode"
 
 // Componente CardCountry que recibe parametros y muestra en la card
-const CardCountry = ({ name }) => {
+const CardCountry = ({ name, emojiU, capital, phone, currency, states }) => {
   return (
     <div className="cardContainer">
       <div className="topContainerCard">
-        <span>emoji</span>
-        <h5>{name}</h5>
+        <span className="cardEmoji">{emojiU && emojiUnicode(emojiU)}</span>
+        <h5 className="cardTitle">{name}</h5>
       </div>
       <div className="bottonContainerCard">
         <div className="topBottonCard">
-          <p>Capital</p>
-          <p>Phone</p>
+          <p className="capitalCard">
+            Capital: {capital ? capital : "Has no capital"}
+          </p>
+          <p className="phoneCard">Phone: +{phone}</p>
         </div>
         <div className="bottonBottonCard">
-          <p>Divisa</p>
-          <select>Estados</select>
+          <p>Divisa: {currency}</p>
+          {states ? (
+            <>
+              <select className="selectCard">
+                <option>States</option>
+                {states.map((state) => (
+                  <option key={state.name}>{state.name}</option>
+                ))}
+              </select>
+            </>
+          ) : (
+            <select>
+              <option>There are no states</option>
+            </select>
+          )}
         </div>
       </div>
     </div>
--- src/components/contentContinents/ContentContinents.jsx
+++ src/components/contentContinents/ContentContinents.jsx
@@ -3,6 +3,7 @@ import CardCountry from "../cardCountry/CardCountry"
 import "./contentsContinents.scss"
 
 const ContentContinents = ({ continents, countries, search, query }) => {
+  console.log(countries)
   return (
     <>
       {query
@@ -13,7 +14,15 @@ const ContentContinents = ({ continents, countries, search, query }) => {
                   <div className="containerContinents" key={n.code}>
                     <h2>{n.name}</h2>
                     <div className="continentContainer">
-                      <CardCountry key={country.code} name={country.name} />
+                      <CardCountry
+                        key={country.code}
+                        capital={country.capital}
+                        emojiU={country.emojiU}
+                        phone={country.phone}
+                        name={country.name}
+                        currency={country.currency}
+                        states={country.states}
+                      />
                     </div>
                   </div>
                 )
@@ -26,7 +35,15 @@ const ContentContinents = ({ continents, countries, search, query }) => {
                 {countries.map(
                   (n) =>
                     n.continent.code === continent.code && (
-                      <CardCountry key={n.code} name={n.name} />
+                      <CardCountry
+                        key={n.code}
+                        capital={n.capital}
+                        emojiU={n.emojiU}
+                        phone={n.phone}
+                        name={n.name}
+                        currency={n.currency}
+                        states={n.states}
+                      />
                     )
                 )}
               </div>
