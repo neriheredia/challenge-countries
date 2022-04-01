ba85733bb98b39dcd972795d57b7125414a4c325
Heredia Pablo Neri <developer.heredia@gmail.com>
2022-04-01 15:56:52 -0300

    Update search.js

--- src/helpers/search.js
+++ src/helpers/search.js
@@ -1,7 +1,7 @@
 //Function search reutilizable
 
-// Realize esta funcion para reutilizarla en donde sea necesario
-// recibe el array a filtrar y la query a buscar
+//Realice esta función para reutilizarla en donde sea necesario
+//recibe el array a filtrar y la query a buscar
 export const search = (data, query) => {
   return data.filter((item) =>
     item.name.toLowerCase().includes(query.toLowerCase())
