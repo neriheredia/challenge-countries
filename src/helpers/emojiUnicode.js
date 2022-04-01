d1e17ac0ac26d279a4e7b195f05a15bc77c28f03
Heredia Pablo Neri <developer.heredia@gmail.com>
2022-04-01 15:56:01 -0300

    Update emojiUnicode.js

--- src/helpers/emojiUnicode.js
+++ src/helpers/emojiUnicode.js
@@ -1,4 +1,4 @@
-// Cree esta funcion que recibe un String en formato Unicode y devuelve el emoji
+// Cree esta función que recibe un String en formato Unicode y devuelve el emoji
 export const emojiUnicode = (unicode) => {
   const characters = unicode.split(" ").map((e) => e.replace("U+", "0x"))
   return String.fromCodePoint(...characters)
