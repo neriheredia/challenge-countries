6653d92ae9934425242bbc0de9db90af53501c86
Heredia Pablo Neri <developer.heredia@gmail.com>
2022-04-01 16:00:54 -0300

    Update useCountries.jsx

--- src/countries/hooks/useCountries.jsx
+++ src/countries/hooks/useCountries.jsx
@@ -1,13 +1,13 @@
-// Primero realice la logica en app y luego hice una refactorizacion
-// y cree este custom hook para que sea mas facil de usar y deje el
-// codigo mas limpio.
+//Primero realice la lógica en app y luego hice una refactorizacion
+//y cree este custom hook para que sea más fácil de usar y deje el
+//código más limpio.
 import { useQuery } from "@apollo/react-hooks"
 import { useEffect, useState } from "react"
 import { GET_CONTINENTS_AND_COUNTRIES } from "../countries-querys"
 
 // Declaracion de la custom hook
 export const useCountries = () => {
-  // Uso el hook de Apollo para traer todos los datos como tambien el loading y el error
+  // Uso el hook de Apollo para traer todos los datos como también el loading y el error
   const { data, loading, error } = useQuery(GET_CONTINENTS_AND_COUNTRIES)
   const [countries, setCountries] = useState([])
   const [languages, setLanguages] = useState([])
@@ -24,12 +24,12 @@ export const useCountries = () => {
     }
   }, [data])
 
-  // Funcion que se ejecuta al hacer click en los botones
+  // Función que se ejecuta al hacer click en los botones
   const handleBtn = () => {
     setAction(!action)
   }
 
-  // Devuelvo un objeto con los metodos y los estados que puede usar los componentes
+  // Devuelvo un objeto con los métodos y los estados que puede usar los componentes
   return {
     countries,
     languages,
